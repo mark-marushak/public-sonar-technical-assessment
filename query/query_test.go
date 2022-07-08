1be8308d716b8e824a3ce47dfdb9fcafa47617ff
mark-marushak <markk.9964@gmail.com>
2022-07-08 10:51:27 +0300

    create and pass hard test for query

--- query/query_test.go
+++ query/query_test.go
@@ -47,3 +47,42 @@ func TestSearchAND(t *testing.T) {
 
 	assert.True(t, group2.search(message), "Case didn't match")
 }
+
+func TestSearchStrongQuery(t *testing.T) {
+
+	g11 := Condition{"juventus"}
+
+	g12 := Group{CondType: OR}
+	g12.Add(Condition{"real madrid"})
+	g12.Add(Condition{"realmadrid"})
+
+	g13 := Condition{"barcelona"}
+
+	g1 := Group{CondType: OR}
+	g1.Add(g11)
+	g1.Add(g12)
+	g1.Add(g13)
+
+	g21 := Group{CondType: OR}
+	g21.Add(Condition{"messi"})
+	g21.Add(Condition{"ronaldo"})
+	g22 := Group{CondType: OR}
+	g22.Add(Condition{"goals"})
+	g22.Add(Condition{"goal"})
+
+	g2 := Group{CondType: AND}
+	g2.Add(g21)
+	g2.Add(g22)
+
+	group := Group{CondType: AND}
+	group.Add(g1)
+	group.Add(g2)
+
+	message := map[string]int{
+		"messi":    1,
+		"goal":     1,
+		"juventus": 1,
+	}
+
+	assert.True(t, group.search(message), "Case didn't match")
+}
