f88c85f4125bdfc719ef590a3183837f395f5d86
mark-marushak <markk.9964@gmail.com>
2022-07-10 21:47:22 +0300

    implement parse for message

--- message/parse.go
+++ message/parse.go
@@ -1,1 +1,13 @@
 package message
+
+import "public-sonar-technical-assessment/parser"
+
+func ParseJson(filepath string) ([]Message, error) {
+	var output = make([]Message, 0, 1000)
+	err := parser.ParseJson(filepath, &output)
+	if err != nil {
+		return nil, err
+	}
+
+	return output, nil
+}
---
+++ message/parse_test.go
@@ -0,0 +1,23 @@
+package message
+
+import (
+	"log"
+	"testing"
+)
+
+func TestParseJson(t *testing.T) {
+	messages, err := ParseJson("/home/sandbox/GolandProjects/public-sonar-technical-assessment/storage/messages/messages.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if len(messages) <= 0 {
+		t.FailNow()
+	}
+
+	for i := 0; i < len(messages); i++ {
+		if messages[i].GetText() == "" {
+			log.Fatal("Empty message")
+		}
+	}
+}
