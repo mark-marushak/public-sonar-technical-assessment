22f0587328a0de441577b5594bde9f4de31fc431
mark-marushak <markk.9964@gmail.com>
2022-07-11 19:41:42 +0300

    fix problem with parsing message
    Problem was with passing []Message through parser.ParseJson()
    but json format looked like []string.
    
    to Fix this issue I changed the output variable to []string
    and create []Message and put every item to it.

--- main.go
+++ main.go
@@ -1,5 +1,72 @@
 package main
 
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"github.com/go-errors/errors"
+	"io"
+	"os"
+	"public-sonar-technical-assessment/message"
+	"public-sonar-technical-assessment/query"
+	"public-sonar-technical-assessment/repository"
+	"public-sonar-technical-assessment/repository/regex"
+)
+
 func main() {
+	pathToQueries := flag.String("cases", "/home/sandbox/GolandProjects/public-sonar-technical-assessment/storage/queries/cases.json", "Path to cases.")
+	pathToMessages := flag.String("messages", "/home/sandbox/GolandProjects/public-sonar-technical-assessment/storage/messages/messages.json", "Path to messages.")
+	pathToOutput := flag.String("outputs", "/home/sandbox/GolandProjects/public-sonar-technical-assessment/storage/output/outputs.json", "Path to output.")
+
+	queries, err := query.ParseJson(*pathToQueries)
+	if err != nil {
+		fmt.Println(errors.Errorf("error while parsing queries: %v", err).ErrorStack())
+		return
+	}
+
+	messages, err := message.ParseJson(*pathToMessages)
+	if err != nil {
+		fmt.Println(errors.Errorf("error while parsing messages: %v", err).ErrorStack())
+		return
+	}
+
+	var builded query.InterfaceNode
+	var repo repository.InterfaceCompareFunc
+	var service repository.ServiceCompare
+	type OutputFormat struct {
+		QueryID  int
+		Messages []string
+	}
+	found := make([]OutputFormat, 0, 10)
+
+	for i := 0; i < len(queries); i++ {
+		builded = query.ParseString(queries[i].Query)
+		outputFormat := OutputFormat{
+			queries[i].CaseID,
+			make([]string, 0, 10),
+		}
+		for j := 0; j < len(messages); j++ {
+			repo = regex.RegexCompare{messages[j].GetText()}
+			service = repository.NewServiceCompare(repo)
+			if builded.Search(service.Compare) {
+				outputFormat.Messages = append(outputFormat.Messages, messages[j].GetText())
+			}
+		}
+		found = append(found, outputFormat)
+	}
+
+	outJson, err := json.Marshal(found)
+	if err != nil {
+		return
+	}
+	outputs, err := os.Create(*pathToOutput)
+	if err != nil {
+		return
+	}
+
+	_, err = io.WriteString(outputs, string(outJson))
+	if err != nil {
+		return
+	}
 
 }
--- message/parse.go
+++ message/parse.go
@@ -3,11 +3,16 @@ package message
 import "public-sonar-technical-assessment/parser"
 
 func ParseJson(filepath string) ([]Message, error) {
-	var output = make([]Message, 0, 1000)
+	var output = make([]string, 0, 1000)
 	err := parser.ParseJson(filepath, &output)
 	if err != nil {
 		return nil, err
 	}
 
-	return output, nil
+	messages := make([]Message, 0, len(output))
+	for i := 0; i < len(output); i++ {
+		messages = append(messages, Message{output[i]})
+	}
+
+	return messages, nil
 }
